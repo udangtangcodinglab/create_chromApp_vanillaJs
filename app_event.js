3164c455f3e0d9a1a38fa26b5a29877ba3614c87
oyunojino <dusrn2xlayjoh@gmail.com>
2025-01-16 11:38:36 +0900

    window 이벤트 관련 내용 작성

--- app_event.js
+++ app_event.js
@@ -4,6 +4,24 @@ function handleTitleClick() {
     title.style.color="blue";
     console.log("click");
 }
+function handleWindowResize() {
+    document.body.style.backgroundColor = "pink";
+}
+function handleWindowCopy() {
+    alert("copier!");
+}
+function handleWindowOffline() {
+    alert("SOS no WIFI");
+}
+function handleWindowOnline() {
+    alert("connect WIFI");
+}
+ 
+// title.addEventListener("click", handleTitleClick);
+title.onclick = handleTitleClick;
+// window.addEventListener("resize", handleWindowResize);
+window.onresize = handleWindowResize;
 
-
-title.addEventListener("click", handleTitleClick);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
