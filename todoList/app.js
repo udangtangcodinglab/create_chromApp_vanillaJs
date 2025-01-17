b351f944c8b20e7e88a4a2dbb0fa8004b4c3eaf4
oyunojino <dusrn2xlayjoh@gmail.com>
2025-01-17 14:51:52 +0900

    로그인 버튼 클릭 시 로그인창은 지우고 완료 문구 출력

--- todoList/app.js
+++ todoList/app.js
@@ -2,13 +2,18 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
 const naver = document.querySelector("a");
+const loginSuccess = document.querySelector("#loginSuccess");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   // submit 이벤트 후 새로고침과 같은 기본 이벤트가 발생하는 것을 막아줌
   event.preventDefault();
-  console.log(event);
   const userName = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   console.log(userName);
+  loginSuccess.innerText = `${userName}님이 로그인했습니다.`;
+  loginSuccess.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function onNaverClick(event) {
