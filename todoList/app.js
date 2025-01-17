4a2f504d653d23b686f9c318e3548e854812b988
oyunojino <dusrn2xlayjoh@gmail.com>
2025-01-17 14:22:25 +0900

    a 태그의 기본 이벤트 막는 방법

--- todoList/app.js
+++ todoList/app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const naver = document.querySelector("a");
+
 function onLoginSubmit(event) {
   // submit 이벤트 후 새로고침과 같은 기본 이벤트가 발생하는 것을 막아줌
   event.preventDefault();
@@ -9,4 +11,11 @@ function onLoginSubmit(event) {
   console.log(userName);
 }
 
+function onNaverClick(event) {
+  event.preventDefault();
+  alert("naver로 이동");
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+naver.addEventListener("click", onNaverClick);
