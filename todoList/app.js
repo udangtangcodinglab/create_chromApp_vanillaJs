3e358303071e53a49ead13e214ddc0bcbe220cc7
oyunojino <dusrn2xlayjoh@gmail.com>
2025-01-17 16:39:03 +0900

    localStorage에 username이 남아있을 때까지 로그인창 hidden처리

--- todoList/app.js
+++ todoList/app.js
@@ -2,18 +2,18 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
 const naver = document.querySelector("a");
-const loginSuccess = document.querySelector("#loginSuccess");
+const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   // submit 이벤트 후 새로고침과 같은 기본 이벤트가 발생하는 것을 막아줌
   event.preventDefault();
-  const userName = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  console.log(userName);
-  loginSuccess.innerText = `${userName}님이 로그인했습니다.`;
-  loginSuccess.classList.remove(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
 function onNaverClick(event) {
@@ -21,6 +21,17 @@ function onNaverClick(event) {
   alert("naver로 이동");
   console.dir(event);
 }
-
-loginForm.addEventListener("submit", onLoginSubmit);
 naver.addEventListener("click", onNaverClick);
+
+function paintGreetings(username) {
+  greeting.innerText = `${username}님이 로그인했습니다.`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
