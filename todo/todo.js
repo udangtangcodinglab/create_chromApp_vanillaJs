1e57ad1fe91ee1f182b306ae6bab279f67beafbd
oyunojino <dusrn2xlayjoh@gmail.com>
2025-01-20 15:42:15 +0900

    li 태그를 추가하여 리스트 추가

--- todo/todo.js
+++ todo/todo.js
@@ -4,12 +4,26 @@ const toDoList = document.getElementById("todo-list");
 
 console.log(toDoForm, toDoList);
 
+function paintToDo(newTodo) {
+  console.log(newTodo);
+
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+
+  console.log(li);
+  //   li.innerText = newTodo;
+}
+
 function handleToDoSubmit(event) {
   // 작성 후 ENTER를 클릭했을 때 새로 고침 차단
   // 참고URL : https://developer.mozilla.org/ko/docs/Web/API/Event/preventDefault
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
